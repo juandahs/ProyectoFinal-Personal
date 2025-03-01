954fda38ef6fa567ad5498b00514e53c1c53bb4a
Luna <osoriomarinluna@gmail.com>
2025-03-01 12:01:16 -0500

    Se crean los servicios TipoImagenDiagnostica e ImagenDiagnostica y se agregan al ServiceExtension

---
+++ ProyectoFinal.Servidor/ImagenDiagnosticaServicio.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Entidades;
+using ProyectoFinal.Repositorio;
+
+namespace ProyectoFinal.Servidor
+{
+    public class ImagenDiagnosticaServicio(Contexto contexto)
+    {
+        private readonly Contexto _contexto = contexto;
+        public IEnumerable<ImagenDiagnostica> ObtenerTodos() => _contexto.ImagenesDiagnosticas.AsNoTracking();
+
+        public ImagenDiagnostica? ObtenerPorId(Guid id) => _contexto.ImagenesDiagnosticas.AsNoTracking().FirstOrDefault(p => p.ImagenDiagnosticaId == id);
+
+        public void Agregar(ImagenDiagnostica imagenDiagnostica)
+        {
+            _contexto.ImagenesDiagnosticas.Add(imagenDiagnostica);
+            _contexto.SaveChanges();
+        }
+
+        public void Actualizar(ImagenDiagnostica imagenDiagnostica)
+        {
+            _ = _contexto.ImagenesDiagnosticas.AsNoTracking().FirstOrDefault(u => u.PacienteId == imagenDiagnostica.ImagenDiagnosticaId) ?? throw new Exception("La imagen diagnostica no existe.");
+
+            _contexto.ImagenesDiagnosticas.Update(imagenDiagnostica);
+            _contexto.SaveChanges();
+        }
+
+        public void Eliminar(Guid id)
+        {
+            var imagenDiagnostica = _contexto.ImagenesDiagnosticas.AsNoTracking().FirstOrDefault(u => u.PacienteId == id);
+            if (imagenDiagnostica != null)
+            {
+                _contexto.ImagenesDiagnosticas.Remove(imagenDiagnostica);
+                _contexto.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ProyectoFinal.Servidor/TipoImagenDiagnosticaServicio.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Entidades;
+using ProyectoFinal.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Servidor
+{
+    public class TipoImagenDiagnosticaServicio(Contexto contexto)
+    {
+        private readonly Contexto _contexto = contexto;
+        public IEnumerable<TipoImagenDiagnostica> ObtenerTodos() => _contexto.TipoImagenesDiagnosticas.AsNoTracking();
+    }
+}
