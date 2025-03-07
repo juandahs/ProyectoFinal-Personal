5baea9ab519d38adcbfa7e3699bd9ae935a649ab
manu mosquera <evelynmmosquera@gmail.com>
2025-03-07 17:29:23 -0500

    Se creo el contralor y vista de TipoExamen,Crear-Listar-Eliminar

--- ProyectoFinal.Servidor/TipoExamenServicio.cs
+++ ProyectoFinal.Servidor/TipoExamenServicio.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ProyectoFinal.Entidades;
 using ProyectoFinal.Repositorio;
 
@@ -13,5 +8,33 @@ namespace ProyectoFinal.Servidor
     {
         private readonly Contexto _contexto = contexto;
         public IEnumerable<TipoExamen> ObtenerTodos() => _contexto.TipoExamenes.AsNoTracking();
+
+        public TipoExamen? ObtenerPorId(Guid tipoExamenId) => _contexto.TipoExamenes.AsNoTracking().FirstOrDefault(p => p.TipoExamenId == tipoExamenId);
+
+        public void Agregar(TipoExamen tipoExamen)
+        {
+            try
+            {
+                _contexto.TipoExamenes.Add(tipoExamen);
+                _contexto.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+       
+
+        public void Eliminar(Guid id)
+        {
+            var tipoExamen = _contexto.TipoExamenes.AsNoTracking().FirstOrDefault(u => u.TipoExamenId == id);
+            if (tipoExamen != null)
+            {
+                _contexto.TipoExamenes.Remove(tipoExamen);
+                _contexto.SaveChanges();
+            }
+        }
+
+
     }
 }
---
+++ ProyectoFinal.VetSite.MVC/Controllers/TipoExamenesController.cs
@@ -0,0 +1,83 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Entidades;
+using ProyectoFinal.Servidor;
+
+namespace ProyectoFinal.VetSite.MVC.Controllers
+{
+    [Authorize]
+    public class TipoExamenesController(
+           TipoExamenServicio tipoExamenServicio
+         , UsuarioServicios usuarioServicios) : Controller
+    {
+
+        private readonly UsuarioServicios _usuarioServicios = usuarioServicios;
+        private readonly TipoExamenServicio _tipoExamenServicio = tipoExamenServicio;
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Gestion de Vacunas";
+            var tipoExamenes = _tipoExamenServicio.ObtenerTodos().ToList();
+            return View(tipoExamenes);
+        }
+
+        [HttpGet] //Obtener los datos necesario de las relaciones
+        public IActionResult Crear()
+        {
+            ViewData["Usuarios"] = _usuarioServicios.ObtenerTodos();
+            return View();
+        }
+        //INSERTAR TIPODEVACUNA
+        [HttpPost]
+        public IActionResult Crear(TipoExamen tipoExamen)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["MensajeError"] = "El registro de la Tipo Examen no es válida. Valide toda la información y trate nuevamente.";
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                var usuarioId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+                tipoExamen.FechaCreacion = DateTime.Now;
+                tipoExamen.FechaModificacion = DateTime.Now;
+                tipoExamen.UsuarioCreacionId = Guid.Parse(usuarioId!);
+                tipoExamen.UsuarioModificacionId = Guid.Parse(usuarioId!);
+
+                _tipoExamenServicio.Agregar(tipoExamen);
+                TempData["MensajeExito"] = "Tipo de examen creado exitosamente.";
+            }
+            catch (Exception e)
+            {
+                TempData["MensajeError"] = $"Ocurrió el siguiente error tratando de Registrar el tipo de examen: {e.Message}";
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Eliminar(Guid id)
+        {
+            try
+            {
+                _tipoExamenServicio.Eliminar(id);
+                TempData["MensajeExito"] = "El propietario ha sido eliminado exitosamente.";
+            }
+            catch (Exception e)
+            {
+                TempData["MensajeError"] = $"Ocurrió un error eliminando el propietario: {e.Message}";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
